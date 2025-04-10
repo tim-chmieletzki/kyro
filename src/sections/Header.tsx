ad1e1352dec0d28a801b2be5f12d8631f1afe822
tim-chmieletzki <timchmieletzki@gmail.com>
2025-04-10 18:08:40 +0200

    Updated header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,15 @@
+import Logo from "@/assets/nexus-logo.svg";
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="top-0 sticky bg-background/80 backdrop-blur-sm p-4">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-2 font-medium text-xl">
+          <Logo />
+          <h1 className="font-medium">Nexus</h1>
+        </div>
+        <nav className="">-</nav>
+      </div>
+    </header>
+  );
 }
