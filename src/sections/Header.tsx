68cbe4df9f39e0e16383273acdfd292de7290e98
tim-chmieletzki <timchmieletzki@gmail.com>
2025-04-13 13:26:41 +0200

    Reset header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,15 +1,7 @@
-import Logo from "@/assets/nexus-logo.svg";
-
 export default function Header() {
   return (
-    <header className="top-0 sticky bg-background/80 backdrop-blur-sm p-4">
-      <div className="flex justify-between items-center">
-        <div className="flex items-center gap-2 font-medium text-xl">
-          <Logo />
-          <h1 className="font-medium">Nexus</h1>
-        </div>
-        <nav className="">-</nav>
-      </div>
+    <header>
+      <span>KYRO</span>
     </header>
   );
 }
